28673f084634ee14e23b8d3ae47bfbe778140dcb
JothaFernandes <66888319+JothaFernandes@users.noreply.github.com>
2021-06-22 20:38:46 -0300

    Update StockRepository.java

--- src/main/java/com/project/bootcamp/repository/StockRepository.java
+++ src/main/java/com/project/bootcamp/repository/StockRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.project.bootcamp.model.Stock;
+import com.project.bootcamp.model.dto.StockDTO;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
