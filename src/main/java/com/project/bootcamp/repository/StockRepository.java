c9050db2daba224874ab7df4c15c16b977fff92c
JothaFernandes <66888319+JothaFernandes@users.noreply.github.com>
2021-06-21 21:43:26 -0300

    Update StockRepository.java

--- src/main/java/com/project/bootcamp/repository/StockRepository.java
+++ src/main/java/com/project/bootcamp/repository/StockRepository.java
@@ -13,9 +13,9 @@ public interface StockRepository extends JpaRepository<Stock, Long> {
 
     Optional<Stock> findByNameAndDate(String name, LocalDate date);
 
-    @Query("select stock from Stock stock where stock.name= :name and stock.date= :date and stock.id <> :id ")
+    @Query("select stock from Stock stock where stock.name = :name and stock.date = :date and stock.id <> :id ")
     Optional<Stock> findByStockUpdate(String name, LocalDate date, Long id);
 
-    @Query("select stock from Stock stock where stock.date= :date")
+    @Query("select stock from Stock stock where stock.date = :date")
     Optional<List<Stock>> findByToday(LocalDate date);
 }
